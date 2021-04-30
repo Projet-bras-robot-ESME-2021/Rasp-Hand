63544388c2aa771d1222a7b92b2897a68659743c
WarwaxHus <80839844+WarwaxHus@users.noreply.github.com>
2021-04-30 13:06:35 +0200

    Add files via upload

--- Code/PSJoystick.py
+++ Code/PSJoystick.py
@@ -11,20 +11,24 @@ class MyController(Controller):
     def __init__(self,motorsDico, **kwargs):
         Controller.__init__(self, **kwargs)
         self.MC=motorsDico
+        self.incre=0
         
     def on_x_press(self):
         print("on_x_press")
+        self.MC["MotorArt1"].MoveMotor(4)
         
 
     def on_x_release(self):
         print("on_x_release")
-        
+        self.MC["MotorArt1"].StopNow()
         
     def on_triangle_press(self):
         print("on_triangle_press")
+        self.MC["MotorArt1"].MoveMotor(15)
 
     def on_triangle_release(self):
         print("on_triangle_release")
+        self.MC["MotorArt1"].StopNow()
 
     def on_circle_press(self):
         print("on_circle_press")
@@ -41,6 +45,37 @@ class MyController(Controller):
     def on_square_release(self):
         print("on_square_release")
         self.MC["MotorBase"].StopNow()
+        
+        
+        
+    def on_L3_up(self, value):
+        print("on_L3_up")
+        if self.incre>2:
+            self.incre-=1
+            self.MC["MotorArt1"].MoveMotor(self.incre)
+
+    def on_L3_down(self, value):
+        print("on_L3_down")
+        if self.incre<12:
+            self.incre+=1
+            self.MC["MotorArt1"].MoveMotor(self.incre)
+
+    def on_L3_left(self, value):
+        print("on_L3_left ")
+
+    def on_L3_right(self, value):
+        print("on_L3_right ")
+
+    def on_L3_y_at_rest(self):
+        """L3 joystick is at rest after the joystick was moved and let go off"""
+        print("on_L3_y_at_rest")
+        self.MC["MotorArt1"].StopNow()
+
+    def on_L3_x_at_rest(self):
+        """L3 joystick is at rest after the joystick was moved and let go off"""
+        print("on_L3_x_at_rest")
+        self.MC["MotorArt1"].StopNow()
+        
         
         
         
--- Code/TheMain.py
+++ Code/TheMain.py
@@ -2,10 +2,12 @@ from Motor_Config import MotorControl
 from PSJoystick import MyController
 
 def init_all():
-    motor_Base=MotorControl(GPIO_Motor=17,initpos=8.5)
+    motor_Base=MotorControl(GPIO_Motor=27,initpos=8.5)
+    motor_Art1=MotorControl(GPIO_Motor=17)
     
     motorDict= {
-        "MotorBase":motor_Base
+        "MotorBase":motor_Base,
+        "MotorArt1":motor_Art1
         }
     
     controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=True,motorsDico=motorDict)
